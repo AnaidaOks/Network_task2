210003da20b4c21738492cb49aec94f70fe3b8b9
Anaida Oksuzyan <aoa2190@gmail.com>
2023-12-18 11:56:20 +0300

    First commit

---
+++ src/main/java/ru/netology/Client.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class Client {
+    public static void main(String[] args) {
+        try(Socket myClientSocket = new Socket("localhost",8080)){
+            PrintWriter writer = new PrintWriter(myClientSocket.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(myClientSocket.getInputStream()));
+            writer.println("Anna");
+            System.out.println(reader.readLine());
+
+           } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        try(ServerSocket mySocket = new ServerSocket(8080)){
+            Socket clientSocket = mySocket.accept();
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("Get new connection!");
+            final String conName = reader.readLine();
+
+            System.out.println(String.format("Hi %s, your port is %d", conName, clientSocket.getPort()));
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
